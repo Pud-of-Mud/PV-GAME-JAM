646ac5928e35869d75b9120222a41a82ec3b170e
messiuuu-goat <minhnguyentd@outlook.com>
2025-12-10 11:25:05 -0500

    Setting up

---
+++ Survival_Game_Project/main.py
@@ -0,0 +1,83 @@
+import pygame
+
+from utils import get_random_position, load_sprite, print_text
+from pygame import (
+    QUIT,
+    KEYDOWN,
+    K_ESCAPE,
+    K_RIGHT,
+    K_LEFT,
+    K_UP,
+    K_DOWN,
+    K_SPACE
+)
+class SpaceRocks:
+    MIN_ASTEROID_DISTANCE = 250
+    def __init__(self):
+        self._init_pygame()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.background = load_sprite("space", False)
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(None, 64)
+        self.message = ""
+        
+    
+        self.asteroids = []
+        self.bullets = []
+        self.spaceship = Spaceship((400, 300), self.bullets.append)
+        
+    
+        for _ in range(6):
+            while True:
+                position = get_random_position(self.screen)
+                if (
+                    position.distance_to(self.spaceship.position)
+                    > self.MIN_ASTEROID_DISTANCE
+                ):
+                    break
+
+            self.asteroids.append(Asteroid(position, self.asteroids.append))
+    def start(self):
+        self.message = "Press 'Space' to begin!"
+        if pygame.key.get_pressed() == K_SPACE:
+            return True
+
+    def main_loop(self):
+        while True:
+            self._handle_input()
+            self._process_game_logic()
+            self._draw()
+
+    def _init_pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Space Rocks")
+
+    def _get_game_objects(self):
+        game_objects = [*self.asteroids, *self.bullets]
+
+        if self.spaceship:
+            game_objects.append(self.spaceship)
+        
+        return game_objects
+    def _handle_input(self):
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                quit()
+            elif (
+                self.spaceship
+                and event.type == KEYDOWN
+                and event.key == K_SPACE
+            ):
+                self.spaceship.shoot()
+
+        is_key_pressed = pygame.key.get_pressed()
+
+        if self.spaceship:
+            if is_key_pressed[K_RIGHT]:
+                self.spaceship.rotate(clockwise=True)
+            elif is_key_pressed[K_LEFT]:
+                self.spaceship.rotate(clockwise=False)
+            if is_key_pressed[K_UP]:
+                self.spaceship.accelerate()
+            elif is_key_pressed[K_DOWN]:
+                self.spaceship.decelerate()
