ac5044068e001db6130d1794b647c6d91bfbc4de
messiuuu-goat <minhnguyentd@outlook.com>
2025-12-18 09:41:16 -0500

    successful condensation of maps. crashing out bc I have other stuff to do

--- map.py
+++ map.py
@@ -5,38 +5,52 @@ class Map:
     def __init__(self, width, height):
         self.width = width / 50
         self.height = height /50
-        self.back0 = None
-        self.tiles = [[None for _ in range(width)] for _ in range(height)]
-        
+        self.back = None
+    
+    def tile_convert(self, map_str, k):
+        tiles = [
+            [k[char] for char in line.strip()] 
+            for line in map_str.strip().splitlines()
+        ]
+        return tiles
+
+    # map1 function: creates a tile-based map layout
+    # To make a new map:
+    # 1. Define a new dictionary mapping characters to background objects
+    # 2. Create a new multi-line string representing the map layout
+    # 3. Use tile_convert to convert the string into a 2D list of background objects
     
     def map1(self):
         k = {
             "0": load_background_obj("soils", "ground", False),
             "l": load_background_obj("grassdirt", "grassdirtLEFT"),
             "r": load_background_obj("grassdirt", "grassdirtRIGHT"),
-            "tr": load_background_obj("grassdirt", "grassdirtTOP_RIGHT"),
-            "tl": load_background_obj("grassdirt", "grassdirtTOP_LEFT"),
-            "br": load_background_obj("grassdirt", "grassdirtBOT_RIGHT"),
-            "bl": load_background_obj("grassdirt", "grassdirtBOT_LEFT"),
+            "1": load_background_obj("grassdirt", "grassdirtTOP_RIGHT"),
+            "2": load_background_obj("grassdirt", "grassdirtTOP_LEFT"),
+            "3": load_background_obj("grassdirt", "grassdirtBOT_RIGHT"),
+            "4": load_background_obj("grassdirt", "grassdirtBOT_LEFT"),
             "x": load_background_obj("grassdirt", "grasstile", False),
             "t": load_background_obj("grassdirt", "grassdirtTOP", False),
             "b": load_background_obj("grassdirt", "grassdirtBOT", False)
         }
         
-        tiles = [
-            [k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"] , k["x"], k["x"], k["x"], k["x"], k["x"], k["x"]], 
-            [k["x"], k["x"], k["x"], k["x"], k["tl"], k["t"], k["t"], k["t"], k["t"], k["t"], k["t"], k["t"], k["t"], k["t"], k["t"], k["t"], k["t"], k["t"] ,k["t"], k["tr"], k["x"], k["x"], k["x"], k["x"]],
-            [k["x"], k["x"], k["x"], k["x"], k["l"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"] ,k["0"], k["r"], k["x"], k["x"], k["x"], k["x"]],
-            [k["x"], k["x"], k["x"], k["x"], k["l"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"] ,k["0"], k["r"], k["x"], k["x"], k["x"], k["x"]], 
-            [k["x"], k["x"], k["x"], k["x"], k["l"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"] ,k["0"], k["r"], k["x"], k["x"], k["x"], k["x"]],
-            [k["x"], k["x"], k["x"], k["x"], k["l"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"] ,k["0"], k["r"], k["x"], k["x"], k["x"], k["x"]],
-            [k["x"], k["x"], k["x"], k["x"], k["l"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"] ,k["0"], k["r"], k["x"], k["x"], k["x"], k["x"]], 
-            [k["x"], k["x"], k["x"], k["x"], k["l"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"] ,k["0"], k["r"], k["x"], k["x"], k["x"], k["x"]],
-            [k["x"], k["x"], k["x"], k["x"], k["l"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"] ,k["0"], k["r"], k["x"], k["x"], k["x"], k["x"]],
-            [k["x"], k["x"], k["x"], k["x"], k["l"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"], k["0"] ,k["0"], k["r"], k["x"], k["x"], k["x"], k["x"]], 
-            [k["x"], k["x"], k["x"], k["x"], k["bl"], k["b"], k["b"], k["b"], k["b"], k["b"], k["b"], k["b"], k["b"], k["b"], k["b"], k["b"], k["b"], k["b"] ,k["b"], k["br"], k["x"], k["x"], k["x"], k["x"]],
-            [k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"], k["x"] , k["x"], k["x"], k["x"], k["x"], k["x"], k["x"]]
-        ] 
+        map1 = '''
+        xxxxxxxxxxxxxxxxxxxxxxxx
+        xxxx2tttttttttttttt1xxxx
+        xxxxl00000000000000rxxxx
+        xxxxl00000000000000rxxxx
+        xxxxl00000000000000rxxxx
+        xxxxl00000000000000rxxxx
+        xxxxl00000000000000rxxxx
+        xxxxl00000000000000rxxxx
+        xxxxl00000000000000rxxxx
+        xxxxl00000000000000rxxxx
+        xxxx4bbbbbbbbbbbbbb3xxxx
+        xxxxxxxxxxxxxxxxxxxxxxxx
+        '''
+
+        tiles = self.tile_convert(map1, k)
+
         return tiles
     
     def obstacle_map1(self):
